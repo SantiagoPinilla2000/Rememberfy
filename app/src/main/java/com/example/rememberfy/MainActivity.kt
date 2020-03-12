886eef6d7a40ff4b0ac8166dc34776e18196101e
Santiago Pinilla <johansantiagopinillauribe@gmail.com>
2020-03-12 07:47:31 -0500

    main

--- app/src/main/java/com/example/rememberfy/MainActivity.kt
+++ app/src/main/java/com/example/rememberfy/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.rememberfy
 
+import android.app.Activity
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Thread.sleep(3000)
+        Thread.sleep(2000)
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
